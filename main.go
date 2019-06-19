d729b7273f4c68f09d29a56b36c3dd98b45c0e44
NickBai <876337011@qq.com>
2019-06-19 17:39:16 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("view/*")
+
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
+}
